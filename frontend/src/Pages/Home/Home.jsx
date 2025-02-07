fea66c8ed4d42a471de6714cb92b9d47509ce801
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-02-07 11:48:22 +0530

    Home

---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBox from '../../components/SearchBox/SearchBox'
+
+const Home = () => {
+  return (
+    <div>
+       <SearchBox/>
+    </div>
+  )
+}
+
+export default Home
